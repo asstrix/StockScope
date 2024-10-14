3be9a0ca9c6dd275ee41a40c2b075f8f9327aaa0
danil <asstrix@protonmail.com>
2024-10-14 14:50:12 +0300

    added get_quotes and calculate_and_display_average_price functions

---
+++ data_download.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+from yfinance import shared
+
+
+def calculate_and_display_average_price(data):
+    if not data.empty:
+        data['MA'] = data['Close'].rolling(window=5).mean()
+        print(data['MA'])
+
+
+def get_quotes(ticker, period):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+calculate_and_display_average_price(get_quotes('AAPL', '1mo'))
---
+++ data_plotting.py
@@ -0,0 +1,5 @@
+
+
+
+def create_and_save_plot(data, ticker, period, filename):
+	pass
