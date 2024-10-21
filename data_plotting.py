bd32d3f47a7b88f6e7507c6c2e69d5847cc3b6e6
Danil <asstrix@protonmail.com>
2024-10-21 22:14:37 +0300

    added create_and_save_plot

--- data_plotting.py
+++ data_plotting.py
@@ -1,11 +1,19 @@
 import plotly.graph_objects as go
-import pandas as pd
+from pathlib import Path
+from data_download import period_spell
+import os
 
 
-# def create_and_save_plot(data, ticker, period, filename=None):
-def create_and_save_plot(data):
+# Export charts as png
+def create_and_save_plot(logger, data, ticker, period):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], mode='lines', name='Close Price'))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['MA'], mode='lines', name='Moving Average'))
-    fig.update_layout(title='Цена акций с течением времени')
-    fig.show()
+    fig.update_layout(title={'text': f"Цены закрытия за {period_spell(period)}", 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'})
+    path = Path(__file__).parent
+    try:
+        os.makedirs(f"{path}/charts", exist_ok=True)
+        fig.write_image(f"{path}/charts/{ticker}{period}.png")
+        logger.debug(f"График сохранен в {path}/charts")
+    except Exception as e:
+        logger.debug(f"Ошибка сохранения графика: {e}")
